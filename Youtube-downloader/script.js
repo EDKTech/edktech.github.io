1e4abd916abb746391407f654f9dc49c84f7de45
EDK Tech <83384667+EDKTech@users.noreply.github.com>
2022-04-04 14:20:01 -0500

    Update script.js

--- Youtube-downloader/script.js
+++ Youtube-downloader/script.js
@@ -1,7 +1,14 @@
 var convertBtn = document.querySelector('.convert-button');
+
+
+
 var URLinput = document.querySelector('.URL-input');convertBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
     sendURL(URLinput.value);
-});function sendURL(URL) {
-    window.location.href = `http://edktech.github.io/Youtube-downloader/download?URL=${URL}`;
-}
+});
+
+app.get('/download', (req,res) => {
+	var URL = req.query.URL;res.header('Content-Disposition', 'attachment; filename="video.mp4"');ytdl(URL, {
+		format:'mp4'
+	}).pipe(res)
+})
